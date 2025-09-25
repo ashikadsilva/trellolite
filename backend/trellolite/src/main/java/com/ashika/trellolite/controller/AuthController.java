47ac0994d56e364a9c6b7c3a74808a49a2f0adf3
ashikadsilva <ashikadsilva18@gmail.com>
2025-09-25 16:10:16 +0530

    Fixed update and get issues in the backend

--- backend/trellolite/src/main/java/com/ashika/trellolite/controller/AuthController.java
+++ backend/trellolite/src/main/java/com/ashika/trellolite/controller/AuthController.java
@@ -21,6 +21,7 @@ public class AuthController {
 
     @PostMapping("/register")
     public String register(@RequestBody User user) {
+        user.setFullName(user.getFirstName() + " " + user.getLastName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         userRepo.save(user);
         return "User registered successfully!";
@@ -51,7 +52,7 @@ public class AuthController {
         return "Hello, anyone can access this!";
     }
 
-    @PreAuthorize("hasRole('admin')")
+    @PreAuthorize("hasRole('user') or hasRole('admin')")
     @GetMapping("/admin/dashboard")
     public Map<String, Object> getAdminDashboard(JwtAuthenticationToken jwtAuth) {
         Map<String, Object> dashboard = new LinkedHashMap<>();
@@ -60,7 +61,7 @@ public class AuthController {
         return dashboard;
     }
 
-    @PreAuthorize("hasRole('admin')")
+    @PreAuthorize("hasRole('user') or hasRole('admin')")
     @GetMapping("/user/profile")
     public Map<String, Object> getUserProfile(JwtAuthenticationToken auth) {
         Map<String, Object> profile = new LinkedHashMap<>();
@@ -71,7 +72,7 @@ public class AuthController {
     }
 
     @PutMapping("/user/profile")
-    @PreAuthorize("hasRole('admin') or hasRole('user')")
+    @PreAuthorize("hasRole('user') or hasRole('admin')")
         public Map<String, Object> updateUserProfile(@RequestBody Map<String, Object> updates, JwtAuthenticationToken jwtAuth) {
 
         String email = jwtAuth.getToken().getClaim("email");
@@ -80,21 +81,25 @@ public class AuthController {
 
 //        Update allowed fields
         if (updates.containsKey("firstName")) {
-            user.setName((String) updates.get("firstName"));
+            user.setFirstName((String) updates.get("firstName"));
         } if (updates.containsKey("lastName")){
-            user.setName((String) updates.get("lastName"));
+            user.setLastName((String) updates.get("lastName"));
+        } if (updates.containsKey("fullName")){
+            user.setFullName((String) updates.get("fullName"));
         }
 
         userRepo.save(user);
 
         Map<String, Object> response = new LinkedHashMap<>();
         response.put("message", "Profile updated successfully");
-        response.put("name", user.getName() + " " + user.getLastName());
+        response.put("firstName", user.getFirstName());
+        response.put("lastName", user.getLastName());
+        response.put("fullName", user.getFullName());
         response.put("email", user.getEmail());
         return response;
     }
 
-    @PreAuthorize("hasRole('user')")
+    @PreAuthorize("hasRole('user') or hasRole('admin')")
     @GetMapping("/user/profile-info")
     public Map<String, Object> getUserProfileInfo(JwtAuthenticationToken auth) {
         String email = auth.getToken().getClaim("email");
@@ -102,8 +107,9 @@ public class AuthController {
                 .orElseThrow(() -> new RuntimeException("User not found"));
 
         Map<String, Object> profile = new LinkedHashMap<>();
-        profile.put("name", user.getName());
-        profile.put("name", user.getName() + " " + user.getLastName());
+        profile.put("firstName", user.getFirstName());
+        profile.put("lastName", user.getLastName());
+        profile.put("fullName", user.getFullName());
         profile.put("email", user.getEmail());
         profile.put("message", "Welcome to your profile!");
         return profile;
